1292f943a7f953ea96c65b28232ea6115eb391e6
DaiyaYoshikawa <31157@ojs.ac.jp>
2020-08-27 11:56:55 +0900

    リッチ化

--- TextEditer31157/Form1.cs
+++ TextEditer31157/Form1.cs
@@ -21,41 +21,16 @@ namespace TextEditer31157 {
 		private void Form1_Load(object sender, EventArgs e) {
 			//ロード
 			initTextEditer();
-			
 		}
 
 		private void ExitToolStripMenuItem_Click(object sender, EventArgs e) {
 			//終了
-			ExitApplication(sender,e);
-		}
-
-		private void ExitApplication(object sender, EventArgs e) {
-			//終了判定メソッド
-			if (rtTextArea.Text != "") {
-				if (Changed) {
-					DialogResult dr = MessageBox.Show
-						(Text + "は更新されています。保存しますか?",
-						Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-					Changed = false;
-					if (dr == DialogResult.Yes) {
-						SaveToolStripMenuItem_Click(sender, e);
-					}
-					else {
-						Application.Exit();
-					}
-				}
-				else {
-					Application.Exit();
-				}
-			}
-			else{
-				Application.Exit();
-			}
+			OpenExitJudge(sender, e, "Exit");
 		}
 
 		private void NamedSaveToolStripMenuItem_Click(object sender, EventArgs e) {
-			//名前を付けて保存
-			sfdFileSave.Filter = "テキスト文書 (.txt)|*.txt|すべてのファイル (*.*)|*.*";
+		//名前を付けて保存
+		sfdFileSave.Filter = "リッチテキスト形式 (.rtf)|*.rtf|すべてのファイル (*.*)|*.*";
 			if (sfdFileSave.ShowDialog() == DialogResult.OK) {
 				FileSave(sfdFileSave.FileName);
 			}
@@ -73,60 +48,45 @@ namespace TextEditer31157 {
 
 		private void FileSave(String fileName) {
 			//保存メソッド
-			using (StreamWriter sw = new StreamWriter
-									(fileName, false, Encoding.GetEncoding("utf-8"))) {
-				sw.WriteLine(rtTextArea.Text);
+			rtTextArea.SaveFile(fileName, RichTextBoxStreamType.RichText);
+			Text = fileName + " - テキストエディタ";
+			initChanged();
+		}
+
+		private void OpenToolStripMenuItem_Click(object sender, EventArgs e) {
+			//開く
+			OpenExitJudge(sender, e, "Open");
+		}
+
+		private void FileOpen() {
+			//開くメソッド
+			if (ofdFileOpen.ShowDialog() == DialogResult.OK) {
+				fileName = ofdFileOpen.FileName;
+				switch (Path.GetExtension(fileName)) {
+					case ".rtf":
+						rtTextArea.LoadFile(fileName, RichTextBoxStreamType.RichText);
+						break;
+					default:
+						rtTextArea.LoadFile(fileName, RichTextBoxStreamType.PlainText);
+						break;
+				}
 				Text = fileName + " - テキストエディタ";
 				initChanged();
 			}
 		}
 
-		private void OpenToolStripMenuItem_Click(object sender, EventArgs e) {
-			//開く
-			if (ofdFileOpen.ShowDialog() == DialogResult.OK) {
-				using (StreamReader sr = new StreamReader
-					(ofdFileOpen.FileName, Encoding.GetEncoding("utf-8"), false)) {
-					rtTextArea.Text = sr.ReadToEnd();
-					fileName = ofdFileOpen.FileName;
-					Text = fileName + " - テキストエディタ";
-					initChanged();
-				}
-			}
-		}
-
 		private void NewToolStripMenuItem_Click(object sender, EventArgs e) {
 			//新規作成
-			if (rtTextArea.Text != "") {
-				if (Changed) {
-					DialogResult dr = MessageBox.Show
-						(Text + "は更新されています。保存しますか?",
-						Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-					if (dr == DialogResult.Yes) {
-						SaveToolStripMenuItem_Click(sender, e);
-					}
-					else {
-						initTextEditer();
-					}
-				}
-				else {
-					initTextEditer();
-				}
-			}
-			else {
-				initTextEditer();
-			}
+			OpenExitJudge(sender, e, "NewCreate");
 		}
 
-		private void SaveJudge() {
-			Changed = true;
-		}
 		private void initTextEditer() {
 			//初期化メソッド
 			rtTextArea.Clear();
-			PasteJudge();
-			DeleteJudge();
 			fileName = "";
 			Text = "無題 - テキストエディタ";
+			PasteJudge();
+			FontColorDelete();
 			initChanged();
 		}
 
@@ -138,17 +98,10 @@ namespace TextEditer31157 {
 			//元に戻す
 			rtTextArea.Undo();
 		}
+
 		private void RedoToolStripMenuItem_Click(object sender, EventArgs e) {
 			//やり直し
 			rtTextArea.Redo();
-		}
-
-		private void rtTextArea_TextChanged(object sender, EventArgs e) {
-			//やり直し・元に戻す・削除・保存の判定
-			RedoToolStripMenuItem.Enabled = rtTextArea.CanRedo ;
-			UndoToolStripMenuItem.Enabled = rtTextArea.CanUndo ;
-			DeleteJudge();
-			SaveJudge();
 		}
 
 		private void CutToolStripMenuItem_Click(object sender, EventArgs e) {
@@ -168,21 +121,16 @@ namespace TextEditer31157 {
 			rtTextArea.Paste();
 		}
 
-		private void PasteJudge() {
-			//貼り付け判定
-			PasteToolStripMenuItem.Enabled = Clipboard.GetDataObject().GetDataPresent(DataFormats.Rtf);
-		}
-
-		private void DeleteJudge() {
-			//削除判定
-			DeleteToolStripMenuItem.Enabled = rtTextArea.Text.Length > 0;
-		}
-
 		private void DeleteToolStripMenuItem_Click(object sender, EventArgs e) {
 			//削除
 			int selectPos = rtTextArea.SelectionStart;
 			try {
-				rtTextArea.Text = rtTextArea.Text.Remove(selectPos, 1);
+				if (rtTextArea.SelectionLength == 0) {
+					rtTextArea.Text = rtTextArea.Text.Remove(selectPos, 1);
+				}
+				else {
+					rtTextArea.Text = rtTextArea.Text.Remove(selectPos, rtTextArea.SelectionLength);
+				}
 				rtTextArea.SelectionStart = selectPos;
 			}
 			catch (ArgumentOutOfRangeException) {
@@ -210,8 +158,22 @@ namespace TextEditer31157 {
 				else {
 					rtTextArea.Font = fdFontDialog.Font;
 				}
-
 			}
+		}
+
+		private void rtTextArea_TextChanged(object sender, EventArgs e) {
+			//やり直し・元に戻す・削除・保存・カラー・フォントの判定
+			RedoToolStripMenuItem.Enabled = rtTextArea.CanRedo;
+			UndoToolStripMenuItem.Enabled = rtTextArea.CanUndo;
+			Changed = true;
+			FontColorDelete();
+		}
+
+		private void FontColorDelete() {
+			//カラー・フォント・削除判定
+			ColorToolStripMenuItem.Enabled = rtTextArea.Text.Length > 0;
+			FontToolStripMenuItem.Enabled = rtTextArea.Text.Length > 0;
+			DeleteToolStripMenuItem.Enabled = rtTextArea.Text.Length > 0;
 		}
 
 		private void rtTextArea_SelectionChanged(object sender, EventArgs e) {
@@ -220,8 +182,57 @@ namespace TextEditer31157 {
 			CopyToolStripMenuItem.Enabled = rtTextArea.SelectedText.Length > 0 ;
 		}
 
+		private void PasteJudge() {
+			//貼り付け判定
+			PasteToolStripMenuItem.Enabled = Clipboard.GetDataObject().GetDataPresent(DataFormats.Rtf);
+		}
+
 		private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
-			ExitApplication(sender, e);
+			//×を押したとき
+			OpenExitJudge(sender, e, "Exit");
+		}
+
+		private void OpenExitJudge(object sender, EventArgs e, string judge) {
+			//終了判定
+			if (rtTextArea.Text != "") {
+				if (Changed) {
+					DialogResult dr = MessageBox.Show
+						(Text + "は更新されています。保存しますか?",
+						Text, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+					Changed = judge != "Exit";
+					if (dr == DialogResult.Yes) {
+						SaveToolStripMenuItem_Click(sender, e);
+						ExitElseFlow(judge);
+						initChanged();
+					}
+					else {
+						ExitElseFlow(judge);
+					}
+				}
+				else {
+					ExitElseFlow(judge);
+				}
+			}
+			else {
+				ExitElseFlow(judge);
+			}
+		}
+
+		private void ExitElseFlow(string judge) {
+			//分岐
+			switch (judge) {
+				case "NewCreate":
+					initTextEditer();
+					break;
+				case "Open":
+					FileOpen();
+					break;
+				case "Exit":
+					Application.Exit();
+					break;
+				default:
+					break;
+			}
 		}
 	}
 }
